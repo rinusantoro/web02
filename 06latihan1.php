2d865d0b43db1788e27a80eb48a61b82dffc3582
rinusantoro <rinusantoro@gmail.com>
2020-04-09 19:47:09 +0700

    PHP Fungsi Array

---
+++ 06latihan1.php
@@ -0,0 +1,20 @@
+<HTML>
+<HEAD>
+<TITLE> Penggunaan Is Array </TITLE>
+</HEAD>
+<BODY>
+<?php
+//$var = array(1,2,3,4,5,6,7);
+$var = "1,2,3,4";
+$scan = is_array($var);
+if ($scan == false) {
+$status = "bukan";
+} else {
+$status = "";
+}
+echo "\$var = array(1,2,3,4,5,6,7)";
+echo "<br>";
+echo "Variabel \$var $status merupakan array";
+?>
+</BODY>
+</HTML>
