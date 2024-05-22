55fe16b17e4773a915183e4e519b52268348a6a0
rinusantoro <rinusantoro@gmail.com>
2024-05-22 19:46:08 +0700

    Updated form search2 n added sorting

--- 13formsearch2.php
+++ 13formsearch2.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 <h3>ContohProgram Mencari record berdasarkan field nama<h3>
-<forma ction="13cari2.php" method="post">
-<label>Masukan nama:</label><input type="text"name="nama"><br>
+<form action="13cari2.php" method="post">
+<label>Masukan nama:</label><input type="text" name="nama"><br>
 <label>Masukan jurusan:</label><input type="text" name="jurusan"><br>
 <p><input type="submit" value="Search"> <input type="reset"
 value="Hapus"></p>
--- 13sorting1.php
+++ 13sorting1.php
@@ -1,22 +1,15 @@
 <?php
-echo
-"<html>
-<body>";
 $koneksi=mysqli_connect("localhost","sonasa","P@ssw0rd","lat_dbase");
-//mysql_select_db("lat_dbase");
-$cari=$_POST['nama'];
-$cari2=$_POST['jurusan'];
-
-$hasil=mysqli_query($koneksi,"select * from tbl_mhs where nama like '%$cari%'or jurusan like'%$cari2%' order by nama asc");
+$hasil=mysqli_query($koneksi,"select * from tbl_mhs order by nim asc"); //menampilkan dari besarke kecil
 echo"<table>
 <tr>
 <th>NIM</th>
-<th>nama</th>
-<th>alamat</th>
-<th>jurusan</th>
+<th>Nama</th>
+<th>Alamat</th>
+<th>Jurusan</th>
 </tr>";
 While($data=mysqli_fetch_array($hasil)){
-echo"<tr>
+echo "<tr>
 <td>".$data['nim']."</td>
 <td>".$data['nama']."</td>
 <td>".$data['alamat']."</td>
